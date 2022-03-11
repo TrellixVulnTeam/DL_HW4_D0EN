6d1ead656253cbab75ea55a645fa8873c7f81531
Yaniv Holder <yaniv.holder@outlook.com>
2022-03-11 10:20:08 +0200

    Added part 2 answers

--- hw4/answers.py
+++ hw4/answers.py
@@ -105,52 +105,47 @@ def part2_vae_hyperparams():
 part2_q1 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+The $\sigma^2$ determines the level of random in the model.
+We can view it as the weight that will determine data-reconstruction loss.
+Low values will make low variability to the data which the model creates 
+so it will be closer to the dataset.
+High values will make high variability to the data which the model creates
+so it will be further away from the dataset.
 
 """
 
 part2_q2 = r"""
 **Your answer:**
 
+1. KL divergence loss purpose is regularization, so the encoder output
+distribution will be similar to standard normal distribution.
+Reconstruction loss purpose is for that the images which are the model output
+will be similar to the original images. 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. The latent-space distribution is affected by the KL loss term by that the
+encoder output distribution will be close to normal distribution.
+
+3. The benefit of this effect is that because the distribution will be close
+to a normal distribution at the middle of the latent space which will make the decoder
+to decode the data generated randomly from the like normal distribution latent space
+so that points which are neighbors will be alike.  
 
 """
 
 part2_q3 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+We maximize the evidence distribution at the start to make the model generated 
+samples to be close to the original samples.
 
 """
 
 part2_q4 = r"""
 **Your answer:**
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+We model the log of the latent-space variance corresponding to an input 
+to achieve stability when we map $[0,1]$ to $[-\infty, log(1)]$.
+This is instead of modeling the variance directly.
 
 """
 
